f45d303654f91f3521073c92967434eb342ab0ee
Alex Cooper <alex@free.vote>
2024-01-11 20:21:40 +0000

    add "social_media" folders back in

---
+++ social_media/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('register/', include('users.urls')),
+    path('login/', auth.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('', include('post.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
